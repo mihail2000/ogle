5bb8e309438be4e429a5cb696fb39cacd6802775
Miika Haapalainen <miika.haapalainen@gmail.com>
2012-09-27 09:42:40 -0700

    drawmain.js now passes jsLint without errors

--- javascripts/drawmain.js
+++ javascripts/drawmain.js
@@ -1,44 +1,35 @@
-{ // Main
-
-require.config({
-    urlArgs: 'bust=' + (new Date()).getTime()    
-});
-
+/*jslint plusplus: true*/
+/*global window, require, define, $, Kinetic, console, Modernizr, URI, document*/
+require.config({urlArgs: 'bust=' + (new Date()).getTime()});
 /*
  *  Entry point for draw screen 
  */
-window.onload = function() {
-    
-    require(['toolbox', 'canvas_handler', 'dropbox_handler', 'fileNameBar', 'javascripts/lib/URI.js'], function(toolbox, canvas_handler, dropbox_handler, fileNameBar) {     
+window.onload = function () {
+    'use strict';
+    require(['toolbox', 'canvas_handler', 'dropbox_handler', 'fileNameBar', 'javascripts/lib/URI.js'], function (toolbox, canvas_handler, dropbox_handler, fileNameBar) {
         // Check if filename was given as a parameter and give file handler the file name.
-        var qs = URI(document.URL).query(true); 
-
-        var fileparameter = qs['filename'];
-        var newParam = qs['newdiagram'];
-        var width = qs['width'];
-        var height = qs['height'];
-        if (width == undefined || height == undefined) {
+        var qs = URI(document.URL).query(true),
+            fileparameter = qs.filename,
+            width = qs.width,
+            height = qs.height;
+        if (width === undefined || height === undefined) {
             width = 800;
             height = 600;
         }
-
         fileNameBar.init(fileparameter);
         toolbox.setcallback(canvas_handler.toolboxcallback);
         canvas_handler.init(width, height);
-        
-        if (fileparameter != undefined) {
+        if (fileparameter !== undefined) {
             dropbox_handler.authenticate();
             dropbox_handler.loadcontents(fileparameter, canvas_handler.loadfilecallback);
         } else {
             canvas_handler.initEmptyCanvas();
         }
     });
-
-  var elem;
-  if (document.getElementById && (elem = document.getElementById('container'))) {
-      if (elem.style) elem.style.cursor = 'crosshair';
-  }
-}
-    
-} // end of Main
-
+    var elem = document.getElementById('container');
+    if (document.getElementById && (elem)) {
+        if (elem.style) {
+            elem.style.cursor = 'crosshair';
+        }
+    }
+};
