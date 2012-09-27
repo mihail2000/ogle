196e882554cbdada15162b08c8bbd8d4be863729
Miika Haapalainen <miika.haapalainen@gmail.com>
2012-09-27 09:26:26 -0700

    arrowController.js fixed for jsLint
    Still gives errors for unused variables on interface functions.

--- javascripts/arrowController.js
+++ javascripts/arrowController.js
@@ -1,68 +1,65 @@
-define (['selectionController'], function(selectionController) {
-
-    var CONST_ID = 'line';
-    
-    var drawing = false;
-
-    function drawArrowElement(drawtolayer, x1, y1, x2, y2) {
-      var points = [];
-      points.push(x1);
-      points.push(y1);
-      points.push(x2);
-      points.push(y2);
-      drawLineElement(drawtolayer, points);  
-    }
-  
+/*
+ * arrowController.js
+ *
+ * Handles necessary actions when user is drawing lines and arrows.
+ */
+/*jslint plusplus: true*/
+/*global define, $, Kinetic, console, Modernizr*/
+define(function () {
+    'use strict';
+    var CONST_ID = 'line',
+        drawing = false,
+        draw_start_x = 0,
+        draw_start_y = 0,
+        draw_end_x = 0,
+        draw_end_y = 0;
     function drawLineElement(drawtolayer, points) {
-      var x = 0;
-      var y = 0;
-      var arrow = new Kinetic.Line({
-        points: points,
-        stroke: "black",
-        strokeWidth: 2
-        });
-      
-      //latestitem = arrow;
-      // add the shape to the layer
-      drawtolayer.add(arrow);
-      drawtolayer.draw();
+        var arrow = new Kinetic.Line({
+                points: points,
+                stroke: "black",
+                strokeWidth: 2
+            });
+        drawtolayer.add(arrow); // add the shape to the layer
+        drawtolayer.draw();
     }
-
+    function drawArrowElement(drawtolayer, x1, y1, x2, y2) {
+        var points = [];
+        points.push(x1);
+        points.push(y1);
+        points.push(x2);
+        points.push(y2);
+        drawLineElement(drawtolayer, points);
+    }
     function startEvent(layer, templayer, x, y) {
         drawing = true;
         draw_start_x = x;
-        draw_start_y = y;              
+        draw_start_y = y;
     }
-    
     function endEvent(layer, templayer, x, y) {
         templayer.removeChildren();
         templayer.draw();
         drawing = false;
-        drawArrowElement(layer, draw_start_x, draw_start_y, draw_end_x, draw_end_y);        
+        drawArrowElement(layer, draw_start_x, draw_start_y, draw_end_x, draw_end_y);
     }
-    
     function moveEvent(layer, templayer, x, y) {
         if (drawing) {
             draw_end_x = x;
             draw_end_y = y;
             templayer.removeChildren();
             templayer.draw();
-            drawArrowElement(templayer, draw_start_x, draw_start_y, x, y, '', '#aaaaaa');                                
+            drawArrowElement(templayer, draw_start_x, draw_start_y, x, y, '', '#aaaaaa');
         }
     }
-
     function getId() {
         return CONST_ID;
     }
-
-
-return {
-    drawLineElement: drawLineElement,
-    drawArrowElement: drawArrowElement,
-    startEvent: startEvent,
-    moveEvent: moveEvent,
-    endEvent: endEvent,
-    keyDownEvent: null,
-    getId: getId    
-    }
+    return {
+        drawLineElement: drawLineElement,
+        drawArrowElement: drawArrowElement,
+        startEvent: startEvent,
+        moveEvent: moveEvent,
+        endEvent: endEvent,
+        keyDownEvent: null,
+        getId: getId
+    };
 });
